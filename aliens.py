20adcaa3b1ce1cb2abc506e3564fd1d16d4c4358
Dabi <22302492@bucks.ac.uk>
2026-09-03 15:43:26 +0100

    Delete aliens.py

--- aliens.py
+++
@@ -1,9 +0,0 @@
-aliens.py
-
-#Load the images
-
-python
-enemy1_img = pygame.image.load("enemy1.png").convert_alpha()  # convert_alpha keeps transparency
-enemy2_img = pygame.image.load("enemy2.png").convert_alpha()
-enemy3_img = pygame.image.load("enemy3.png").convert_alpha()
-
